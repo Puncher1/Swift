c26deedb293efdcd6bad8de80ea60421f02ad2fe
Puncher <65789180+Puncher1@users.noreply.github.com>
2021-11-07 22:34:29 +0100

    Added text field with rounded corners

--- AppTesting/AppTesting/ContentView.swift
+++ AppTesting/AppTesting/ContentView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World!").padding()
+        Text("Hello, world!").padding().background(Color.green).clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/9.0/*@END_MENU_TOKEN@*/))
+        
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
