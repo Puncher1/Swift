ada000e4a57930ad06bec3948eae965f951c3363
Puncher <65789180+Puncher1@users.noreply.github.com>
2021-11-07 21:01:12 +0100

    Created AppTesting

---
+++ AppTesting/AppTesting/AppTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTestingApp.swift
+//  AppTesting
+//
+//  Created by Andrin Schaller on 07.11.21.
+//
+
+import SwiftUI
+
+@main
+struct AppTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
