e05cbd362f8487b483deb4c9ec81a41a84924677
Puncher <65789180+Puncher1@users.noreply.github.com>
2021-11-14 22:12:52 +0100

    Created project grades

---
+++ grades/grades/gradesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradesApp.swift
+//  Grades
+//
+//  Created by Andrin Schaller on 14.11.21.
+//
+
+import SwiftUI
+
+@main
+struct GradesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
