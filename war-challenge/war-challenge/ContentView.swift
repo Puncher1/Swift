2f1416381ba1a9202191a4154205127e9a6ed0b6
Puncher <65789180+Puncher1@users.noreply.github.com>
2021-11-14 18:36:00 +0100

    Finished war game

--- war-challenge/war-challenge/ContentView.swift
+++ war-challenge/war-challenge/ContentView.swift
@@ -8,6 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    /*
+     @State:
+        1. You can change the value in the var
+        2. In 'View', every references to '@State' roperties will update
+           automatically
+     
+    private:
+        To set the access level. Means those variables are only available
+        for 'ContentView'
+     
+    */
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+
     var body: some View {
         
         ZStack{
@@ -21,14 +38,36 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Image("card3")
+                    Image(playerCard)
                     Spacer()
-                    Image("card4")
+                    Image(cpuCard)
                     Spacer()
                     
                 }
                 Spacer()
-                Image("dealbutton")
+                Button(action: {
+                    
+                    // Generate random number in range (2, 14)
+                    let randomPlayerNum = Int.random(in: 2...14)
+                    let randomCPUNum = Int.random(in: 2...14)
+                    
+                    // Update cards
+                    playerCard = "card\(randomPlayerNum)"
+                    cpuCard = "card\(randomCPUNum)"
+                    
+                    // Update scores
+                    if randomPlayerNum > randomCPUNum{
+                        playerScore += 1
+                    }
+                    else if randomCPUNum > randomPlayerNum{
+                        cpuScore += 1
+                    }
+                    
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                
                 Spacer()
                 HStack{
                     Spacer()
@@ -38,7 +77,7 @@ struct ContentView: View {
                             .fontWeight(.bold)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 2)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
@@ -49,7 +88,7 @@ struct ContentView: View {
                             .fontWeight(.bold)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 2)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
